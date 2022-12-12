59532beaa620fca8c8b3aae99f4b4f20e0209bb2
Rick Mather <rick.mather@cef.co.uk>
2022-12-12 10:29:17 +0000

    feat: solve day 8

--- src/days/08/index.test.ts
+++ src/days/08/index.test.ts
@@ -1,6 +1,12 @@
 import * as path from "path";
 
-import { getPart1Answer, getPart2Answer } from ".";
+import {
+  countVisibleTrees,
+  getPart1Answer,
+  getPart2Answer,
+  getScenicScore,
+  parseInput,
+} from ".";
 import { readFileToString } from "../../util";
 
 describe("Day 08", () => {
@@ -9,11 +15,45 @@ describe("Day 08", () => {
   );
   const realInput = readFileToString(path.join(__dirname, "input/input.txt"));
 
+  const expectedParsedInput = [
+    [3, 0, 3, 7, 3],
+    [2, 5, 5, 1, 2],
+    [6, 5, 3, 3, 2],
+    [3, 3, 5, 4, 9],
+    [3, 5, 3, 9, 0],
+  ];
+
+  it("parses input data", () => {
+    const data = parseInput(sampleInput);
+    expect(data).toEqual(expectedParsedInput);
+  });
+
+  it("counts visible trees from sample dataset", () => {
+    const visibleTrees = countVisibleTrees(expectedParsedInput);
+
+    expect(visibleTrees).toEqual(21);
+  });
+
+  const scenicScoreTestCases = [
+    // [1, 2, 4],
+    [3, 2, 8],
+  ];
+
+  test.each(scenicScoreTestCases)(
+    "finds scenic score row: %p, col: %p, expected score: %p",
+    (row, col, expectedScore) => {
+      const scenicScore = getScenicScore(expectedParsedInput, row, col);
+      expect(scenicScore).toEqual(expectedScore);
+    }
+  );
+
   it("solves part 1", () => {
-    getPart1Answer(realInput);
+    const visibileTrees = getPart1Answer(realInput);
+    expect(visibileTrees).toEqual(1809);
   });
 
   it("solves part 2", () => {
-    getPart2Answer(realInput);
+    const bestScenicScore = getPart2Answer(realInput);
+    expect(bestScenicScore).toEqual(3130512);
   });
 });
--- src/days/08/index.ts
+++ src/days/08/index.ts
@@ -1,7 +1,155 @@
-export const getPart1Answer = (input: string): void => {
-  return;
+export const parseInput = (input: string): number[][] =>
+  input
+    .trim()
+    .split("\n")
+    .map((line) => {
+      return line.split("").map(Number);
+    });
+
+export const countVisibleTrees = (trees: number[][]): number => {
+  let visible = 0;
+  for (let row = 0; row < trees.length; row++) {
+    for (let col = 0; col < trees[row].length; col++) {
+      visible +=
+        isVisibleFromLeft(trees, row, col) ||
+        isVisibleFromRight(trees, row, col) ||
+        isVisibleFromTop(trees, row, col) ||
+        isVisibleFromBottom(trees, row, col)
+          ? 1
+          : 0;
+    }
+  }
+  return visible;
 };
 
-export const getPart2Answer = (input: string): void => {
-  return;
+export const getScenicScore = (
+  trees: number[][],
+  row: number,
+  col: number
+): number => {
+  return (
+    countVisibleTreesToLeft(trees, row, col) *
+    countVisibleTreesToRight(trees, row, col) *
+    countVisibleTreesToTop(trees, row, col) *
+    countVisibleTreesToBottom(trees, row, col)
+  );
+};
+
+// lots of refactoring could be done here as these methods
+// all reuse the same formula for left / right / up / down
+
+const countVisibleTreesToTop = (
+  trees: number[][],
+  row: number,
+  col: number
+): number => {
+  const lineOfSight = trees
+    .map((i) => i.slice(col, col + 1))
+    .map((x) => x[0])
+    .slice(0, row)
+    .reverse();
+  return getVisibleTreeCount(lineOfSight, trees[row][col]);
+};
+
+const countVisibleTreesToBottom = (
+  trees: number[][],
+  row: number,
+  col: number
+): number => {
+  const lineOfSight = trees
+    .map((i) => i.slice(col, col + 1))
+    .map((x) => x[0])
+    .slice(row + 1);
+  return getVisibleTreeCount(lineOfSight, trees[row][col]);
+};
+
+const countVisibleTreesToLeft = (
+  trees: number[][],
+  row: number,
+  col: number
+): number => {
+  const lineOfSight = trees[row].slice(0, col).reverse();
+  return getVisibleTreeCount(lineOfSight, trees[row][col]);
+};
+
+const countVisibleTreesToRight = (
+  trees: number[][],
+  row: number,
+  col: number
+): number => {
+  const lineOfSight = trees[row].slice(col + 1);
+  return getVisibleTreeCount(lineOfSight, trees[row][col]);
+};
+
+const getVisibleTreeCount = (trees: number[], currentTreeHeight: number) => {
+  let visible = 0;
+
+  while (trees.length) {
+    visible++;
+    const value = trees.shift()!;
+    if (value >= currentTreeHeight) break;
+  }
+
+  return visible;
+};
+
+const isVisibleFromLeft = (
+  trees: number[][],
+  row: number,
+  col: number
+): boolean => {
+  return trees[row].slice(0, col).every((height) => height < trees[row][col]);
+};
+
+const isVisibleFromRight = (
+  trees: number[][],
+  row: number,
+  col: number
+): boolean => {
+  return trees[row].slice(col + 1).every((height) => height < trees[row][col]);
+};
+
+const isVisibleFromTop = (
+  trees: number[][],
+  row: number,
+  col: number
+): boolean => {
+  return trees
+    .map((i) => i.slice(col, col + 1))
+    .map((x) => x[0])
+    .slice(0, row)
+    .every((height) => height < trees[row][col]);
+};
+
+const isVisibleFromBottom = (
+  trees: number[][],
+  row: number,
+  col: number
+): boolean => {
+  return trees
+    .map((i) => i.slice(col, col + 1))
+    .map((x) => x[0])
+    .slice(row + 1)
+    .every((height) => height < trees[row][col]);
+};
+
+export const getPart1Answer = (input: string): number => {
+  return countVisibleTrees(parseInput(input));
+};
+
+export const getPart2Answer = (input: string): number => {
+  const trees = parseInput(input);
+
+  let bestScenicScore = 0;
+
+  for (let row = 0; row < trees.length; row++) {
+    for (let col = 0; col < trees[row].length; col++) {
+      const scenicScore = getScenicScore(trees, row, col);
+      if (scenicScore > bestScenicScore) {
+        bestScenicScore = scenicScore;
+      }
+    }
+  }
+
+  return bestScenicScore;
 };
