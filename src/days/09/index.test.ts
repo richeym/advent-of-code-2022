bc1b1b48c828046e4c0f781f922a075d296dcfa5
Rick Mather <rick.mather@cef.co.uk>
2022-12-12 14:13:44 +0000

    feat: solve day 9 part 1

--- src/days/09/index.test.ts
+++ src/days/09/index.test.ts
@@ -1,16 +1,132 @@
 import * as path from "path";
 
-import { getPart1Answer, getPart2Answer } from ".";
+import {
+  Coordinate,
+  getPart1Answer,
+  getPart2Answer,
+  move,
+  runSimulation,
+  parseInput,
+  Vector,
+} from ".";
 import { readFileToString } from "../../util";
 
 describe("Day 09", () => {
   const sampleInput = readFileToString(
     path.join(__dirname, "input/sample-input.txt")
   );
+
+  const expectedParsedInput: Vector[] = [
+    {
+      direction: "R",
+      distance: 4,
+    },
+    {
+      direction: "U",
+      distance: 4,
+    },
+    {
+      direction: "L",
+      distance: 3,
+    },
+    {
+      direction: "D",
+      distance: 1,
+    },
+    {
+      direction: "R",
+      distance: 4,
+    },
+    {
+      direction: "D",
+      distance: 1,
+    },
+    {
+      direction: "L",
+      distance: 5,
+    },
+    {
+      direction: "R",
+      distance: 2,
+    },
+  ];
+
   const realInput = readFileToString(path.join(__dirname, "input/input.txt"));
 
+  it("parses input", () => {
+    const input = parseInput(sampleInput);
+    expect(input).toEqual(expectedParsedInput);
+  });
+
+  const headMoveTestCases: (Vector | Coordinate)[][] = [
+    [
+      { direction: "R", distance: 1 },
+      { x: 1, y: 0 },
+    ],
+    [
+      { direction: "U", distance: 2 },
+      { x: 0, y: -2 },
+    ],
+    [
+      { direction: "D", distance: 3 },
+      { x: 0, y: 3 },
+    ],
+    [
+      { direction: "L", distance: 4 },
+      { x: -4, y: 0 },
+    ],
+  ];
+
+  test.each(headMoveTestCases)("Moves head %p", (vector, coord) => {
+    const h: Coordinate = { x: 0, y: 0 };
+    const t: Coordinate = { x: 0, y: 0 };
+    const expectedMoveResult = move(vector as Vector, h, t);
+    expect(expectedMoveResult.h).toEqual(coord as Coordinate);
+  });
+
+  const tailNotFollowingHeadOnInitialMoveTestCases: (Vector | Coordinate)[][] =
+    [
+      [
+        { direction: "R", distance: 1 },
+        { x: 0, y: 0 },
+      ],
+      [
+        { direction: "L", distance: 1 },
+        { x: 0, y: 0 },
+      ],
+      [
+        { direction: "U", distance: 1 },
+        { x: 0, y: 0 },
+      ],
+      [
+        { direction: "D", distance: 1 },
+        { x: 0, y: 0 },
+      ],
+      [
+        { direction: "R", distance: 3 },
+        { x: 2, y: 0 },
+      ],
+    ];
+
+  test.each(tailNotFollowingHeadOnInitialMoveTestCases)(
+    "tail does not follows if adjacent. Direction: %p, End position: %p",
+    (vector, expectedPosition) => {
+      const h: Coordinate = { x: 0, y: 0 };
+      const t: Coordinate = { x: 0, y: 0 };
+
+      const expectedMoveResult = move(vector as Vector, h, t);
+      expect(expectedMoveResult.t).toEqual(expectedPosition);
+    }
+  );
+
+  it("solves sample input", () => {
+    const result = runSimulation(expectedParsedInput);
+    expect(result).toEqual(13);
+  });
+
   it("solves part 1", () => {
-    getPart1Answer(realInput);
+    const result = getPart1Answer(realInput);
+    expect(result).toEqual(1);
   });
 
   it("solves part 2", () => {
--- src/days/09/index.ts
+++ src/days/09/index.ts
@@ -1,5 +1,110 @@
-export const getPart1Answer = (input: string): void => {
-  return;
+export interface Vector {
+  direction: string;
+  distance: number;
+}
+
+export interface Coordinate {
+  x: number;
+  y: number;
+}
+
+export interface MoveResult {
+  h: Coordinate;
+  t: Coordinate;
+  tVisited: Coordinate[];
+}
+
+export const runSimulation = (vectors: Vector[]): number => {
+  let moveResult: MoveResult = {
+    h: { x: 0, y: 0 },
+    t: { x: 0, y: 0 },
+    tVisited: [],
+  };
+
+  const visited: Coordinate[] = [{ x: 0, y: 0 }];
+
+  for (let vector of vectors) {
+    moveResult = move(vector, moveResult.h, moveResult.t);
+
+    for (let visitedThisMove of moveResult.tVisited) {
+      if (
+        !visited.find(
+          (coord) =>
+            coord.x === visitedThisMove.x && coord.y === visitedThisMove.y
+        )
+      ) {
+        visited.push(visitedThisMove);
+      }
+    }
+  }
+
+  return visited.length;
+};
+
+export const move = (
+  vector: Vector,
+  h: Coordinate,
+  t: Coordinate
+): MoveResult => {
+  let currentHx: number = h.x;
+  let currentHy: number = h.y;
+  const tVisited: Coordinate[] = [];
+
+  let currentH: Coordinate = h;
+  let lastH: Coordinate = h;
+  let currentT: Coordinate = t;
+
+  for (let i = 0; i < vector.distance; i++) {
+    lastH = currentH;
+
+    if (vector.direction === "R") {
+      currentHx++;
+    }
+    if (vector.direction === "L") {
+      currentHx--;
+    }
+    if (vector.direction === "U") {
+      currentHy--;
+    }
+    if (vector.direction === "D") {
+      currentHy++;
+    }
+
+    currentH = { x: currentHx, y: currentHy };
+
+    if (
+      Math.abs(currentH.x - currentT.x) > 1 ||
+      Math.abs(currentH.y - currentT.y) > 1
+    ) {
+      currentT = lastH;
+      tVisited.push(currentT);
+    }
+  }
+
+  return {
+    h: currentH,
+    t: currentT,
+    tVisited: tVisited,
+  };
+};
+
+export const parseInput = (input: string): Vector[] => {
+  return input
+    .trim()
+    .split("\n")
+    .map((line) => {
+      const items = line.split(" ");
+
+      return {
+        direction: items[0],
+        distance: parseInt(items[1]),
+      };
+    });
+};
+
+export const getPart1Answer = (input: string): number => {
+  const instructions = parseInput(input);
+  return runSimulation(instructions);
 };
 
 export const getPart2Answer = (input: string): void => {
