b67c7945552f6452f23deafe61acb9d6841bcf25
Rick Mather <rick.mather@cef.co.uk>
2022-12-21 15:32:04 +0000

    chore: skipping day 12 for now

--- src/days/12/index.test.ts
+++ src/days/12/index.test.ts
@@ -1,24 +1,34 @@
 import * as path from "path";
 
-import { getPart1Answer, getPart2Answer, parseInput } from ".";
+import { getPart1Answer, getPart2Answer, parseInput, RouteFinder } from ".";
 import { readFileToString } from "../../util";
 
 describe("Day 12", () => {
   const sampleInput = readFileToString(
     path.join(__dirname, "input/sample-input.txt")
   );
+
+  const sampleInputAfterParsing = [
+    ["S", "a", "b", "q", "p", "o", "n", "m"],
+    ["a", "b", "c", "r", "y", "x", "x", "l"],
+    ["a", "c", "c", "s", "z", "E", "x", "k"],
+    ["a", "c", "c", "t", "u", "v", "w", "j"],
+    ["a", "b", "d", "e", "f", "g", "h", "i"],
+  ];
+
   const realInput = readFileToString(path.join(__dirname, "input/input.txt"));
 
   it("parses input", () => {
     const parsedInput = parseInput(sampleInput);
 
-    expect(parsedInput).toEqual([
-      ["S", "a", "b", "q", "p", "o", "n", "m"],
-      ["a", "b", "c", "r", "y", "x", "x", "l"],
-      ["a", "c", "c", "s", "z", "E", "x", "k"],
-      ["a", "c", "c", "t", "u", "v", "w", "j"],
-      ["a", "b", "d", "e", "f", "g", "h", "i"],
-    ]);
+    expect(parsedInput).toEqual(sampleInputAfterParsing);
+  });
+
+  it.skip("solves sample input", () => {
+    const routeFinder = new RouteFinder(sampleInputAfterParsing);
+    const output = routeFinder.plotShortestRoute();
+
+    expect(output).toBe(31);
   });
 
   it("solves part 1", () => {
--- src/days/12/index.ts
+++ src/days/12/index.ts
@@ -1,5 +1,39 @@
 export type HeightMap = string[][];
 
+export class RouteFinder {
+  readonly destinationRow: number;
+  readonly destinationCol: number;
+  shortestRoute: number | undefined;
+
+  constructor(public readonly map: HeightMap) {
+    const { row, col } = findElementInArray(map, "E");
+    this.destinationRow = row;
+    this.destinationCol = col;
+  }
+
+  plotShortestRoute = (): number => {
+    this.shortestRoute = Number.MAX_SAFE_INTEGER;
+    let { row, col } = findElementInArray(this.map, "S");
+
+    return 0;
+  };
+}
+
+const findElementInArray = (
+  arr: string[][],
+  element: string
+): { row: number; col: number } => {
+  for (let row = 0; row < arr.length; row++) {
+    for (let col = 0; col < arr[row].length; col++) {
+      if (arr[row][col] === element) {
+        return { row, col };
+      }
+    }
+  }
+
+  throw Error("couldnt find element");
+};
+
 export const parseInput = (input: string): HeightMap => {
   return input
     .trim()
